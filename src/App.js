02683b1219bc8752278b33420abb894e7b7e638e
Arun7398 <arun01023@gmail.com>
2023-04-06 12:31:19 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Class from "./pages/home/Class";
+import Stucapstone from "./pages/capstone/Stucapstone";
+import Capstone from "./pages/capstone/Capstone";
+import Batches from "./pages/batch/Batches";
+import Event from "./pages/eventManage/Events";
 function App() {
+  const token = window.localStorage.getItem("app-token");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mx-5">
+      <Routes>
+        {token ? (
+          <>
+            <Route path="/" element={<Login />} />
+            <Route exact path="/home" element={<Class />} />
+            <Route path="/eventManage" element={<Event/> }/>
+            <Route path="/stucapstone" element={<Stucapstone />} />
+            <Route path="/capstone/:userId" element={<Capstone />} />
+            <Route path="/batchCreate" element={<Batches />} />
+          </>
+        ) : (
+          <Route path="*" element={<Login />} />
+        )}
+      </Routes>
     </div>
   );
 }
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API='https://zen-class-event-for-student-backend.onrender.com/api'
